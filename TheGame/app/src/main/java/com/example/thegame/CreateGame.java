718de7e681b4a0a1333dc51708b036c97d91e668
KillGuil <selin.ispir@etu.univ-lyon1.fr>
2022-04-07 11:23:03 +0200

    Commit intégration Serveur Android, Client Android

---
+++ TheGame/app/src/main/java/com/example/thegame/CreateGame.java
@@ -0,0 +1,14 @@
+package com.example.thegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_game);
+    }
+}
---
+++ The_Game_Client/app/src/main/java/com/example/the_game_client/CreateGame.java
@@ -0,0 +1,14 @@
+package com.example.the_game_client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_game);
+    }
+}
